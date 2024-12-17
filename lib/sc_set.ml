27a7fac25252c904bdf88774e8a40cb50c299e65
lunghr <myilanka8@gmail.com>
2024-12-17 20:46:23 +0300

    feat: add .ocamlformat

--- lib/sc_set.ml
+++ lib/sc_set.ml
@@ -1,56 +1,49 @@
 (**)
-type 'a entry = {hash: int; value: 'a}
-type 'a chain =
-    | Empty
-    | Node of {
-        entry: 'a entry;
-        next: 'a chain
-    }
-type 'a hashSet = {
-    size: int;
-    chains: 'a chain array
-}
+type 'a entry = { hash : int; value : 'a }
+type 'a chain = Empty | Node of { entry : 'a entry; next : 'a chain }
+type 'a hashSet = { size : int; chains : 'a chain array }
 
-
- let rec string_of_chain = function
+let rec string_of_chain = function
   | Empty -> ""
   | Node node ->
-      let entry_str = Printf.sprintf "{hash = %d; value = %d}" node.entry.hash node.entry.value in
-      entry_str ^ " -> " ^ (string_of_chain node.next)
+      let entry_str =
+        Printf.sprintf "{hash = %d; value = %d}" node.entry.hash
+          node.entry.value
+      in
+      entry_str ^ " -> " ^ string_of_chain node.next
 
+let string_of_hashSet hashSet =
+  Array.fold_left
+    (fun acc chain -> acc ^ string_of_chain chain ^ "\n")
+    "" hashSet.chains
 
- let string_of_hashSet hashSet =
-  Array.fold_left (fun acc chain ->
-    acc ^ (string_of_chain chain) ^ "\n"
-  ) "" hashSet.chains
-
-let create size = {
-    size = size;
-    chains = Array.make size Empty
-}
-
-let hash value=
-    Hashtbl.hash value
+let create size = { size; chains = Array.make size Empty }
+let hash value = Hashtbl.hash value
 
 let add hashSet value =
-    let key = hash value in
-    let index = key mod hashSet.size in
+  let key = hash value in
+  let index = key mod hashSet.size in
 
-    let rec update_chain chain =
-        match chain with
-        | Empty -> Node {entry = {hash = key; value = value}; next = Empty}
-        | Node node ->
-            match node.entry.hash with
-            |k when k = key -> Node {entry = {hash = key; value = value}; next = node.next}
-            |_ -> Node {entry = node.entry; next = update_chain node.next}
-    in
+  let rec update_chain chain =
+    match chain with
+    | Empty -> Node { entry = { hash = key; value }; next = Empty }
+    | Node node -> (
+        match node.entry.hash with
+        | k when k = key ->
+            Node { entry = { hash = key; value }; next = node.next }
+        | _ -> Node { entry = node.entry; next = update_chain node.next })
+  in
 
-    let new_chains = Array.mapi
-        (fun i c -> if i = index then update_chain c else c)
-        hashSet.chains
-    in
-    {hashSet with chains = new_chains}
-
+  let new_chains =
+    Array.mapi
+      (fun i c ->
+        if i = index then
+          update_chain c
+        else
+          c)
+      hashSet.chains
+  in
+  { hashSet with chains = new_chains }
 
 let remove hashSet value =
   let key = hash value in
@@ -64,19 +57,23 @@ let remove hashSet value =
     size = hashSet.size;
     chains =
       Array.mapi
-        (fun i chain -> if i = index then remove_from_chain chain else chain)
+        (fun i chain ->
+          if i = index then
+            remove_from_chain chain
+          else
+            chain)
         hashSet.chains;
   }
 
 let setHas hashSet value =
-    let key = hash value in
-    let index = key mod hashSet.size in
-    let rec loop = function
-        | Empty -> false
-        | Node node when node.entry.hash = key -> true
-        | Node node -> loop node.next
-    in
-    loop hashSet.chains.(index)
+  let key = hash value in
+  let index = key mod hashSet.size in
+  let rec loop = function
+    | Empty -> false
+    | Node node when node.entry.hash = key -> true
+    | Node node -> loop node.next
+  in
+  loop hashSet.chains.(index)
 
 let filter hashSet predicate =
   let filter_chain chain =
@@ -88,20 +85,20 @@ let filter hashSet predicate =
     in
     aux chain
   in
-  {
-    size = hashSet.size;
-    chains = Array.map filter_chain hashSet.chains;
-  }
+  { size = hashSet.size; chains = Array.map filter_chain hashSet.chains }
 
 let map f hashSet =
   let rec map_chain acc = function
     | Empty -> acc
     | Node { entry; next } ->
-        let new_entry = { hash = hash (f entry.value); value = f entry.value } in
+        let new_entry =
+          { hash = hash (f entry.value); value = f entry.value }
+        in
         map_chain (add acc new_entry.value) next
   in
-  Array.fold_left map_chain { size = hashSet.size; chains = Array.make hashSet.size Empty } hashSet.chains
-
+  Array.fold_left map_chain
+    { size = hashSet.size; chains = Array.make hashSet.size Empty }
+    hashSet.chains
 
 let fold_left f init hashSet =
   let rec fold_chain acc = function
@@ -131,36 +128,35 @@ let fold_right f init hashSet =
 
   fold_array init 0
 
+let rec mergeChains (chain1 : 'a chain) (chain2 : 'a chain) : 'a chain =
+  match (chain1, chain2) with
+  | Empty, _ -> chain2
+  | _, Empty -> chain1
+  | Node { entry = entry1; next = next1 }, Node { entry = entry2; next = next2 }
+    ->
+      Node
+        {
+          entry = entry1;
+          next = mergeChains next1 (Node { entry = entry2; next = next2 });
+        }
 
-let rec mergeChains (chain1: 'a chain) (chain2: 'a chain) : 'a chain =
-    match chain1, chain2 with
-    | Empty, _ -> chain2
-    | _, Empty -> chain1
-    | Node { entry = entry1; next = next1 }, Node { entry = entry2; next = next2 } ->
-        Node { entry = entry1; next = mergeChains next1 (Node { entry = entry2; next = next2 }) }
-
-
-let merge (set1: 'a hashSet) (set2: 'a hashSet) : 'a hashSet =
-    let newChains = Array.init set1.size (fun i ->
+let merge (set1 : 'a hashSet) (set2 : 'a hashSet) : 'a hashSet =
+  let newChains =
+    Array.init set1.size (fun i ->
         let chain1 = set1.chains.(i) in
         let chain2 = set2.chains.(i) in
-        mergeChains chain1 chain2
-    ) in
-    {
-        size = set1.size;
-        chains = newChains
-    }
-
-
+        mergeChains chain1 chain2)
+  in
+  { size = set1.size; chains = newChains }
 
 let rec compare_chains chain1 chain2 =
   match (chain1, chain2) with
-  | (Empty, Empty) -> true
-  | (Empty, _) | (_, Empty) -> false
-  | (Node { entry = e1; next = n1 }, Node { entry = e2; next = n2 }) ->
+  | Empty, Empty -> true
+  | Empty, _ | _, Empty -> false
+  | Node { entry = e1; next = n1 }, Node { entry = e2; next = n2 } ->
       e1.hash = e2.hash && e1.value = e2.value && compare_chains n1 n2
 
-let compare_sets (set1: 'a hashSet) (set2: 'a hashSet) : bool =
+let compare_sets (set1 : 'a hashSet) (set2 : 'a hashSet) : bool =
   if set1.size != set2.size then
     false
   else
--- test/test_second_lab.ml
+++ test/test_second_lab.ml
@@ -1,7 +1,6 @@
 open Sc_set
 open OUnit2
 
-
 let len = 10
 let set = create len
 let set = add set 1
@@ -9,176 +8,170 @@ let set = add set 2
 let set = add set 3
 let set = add set 4
 let set = add set 5
-
 let string_set = create len
 let string_set = add string_set "a"
 let string_set = add string_set "b"
 let string_set = add string_set "c"
 let string_set = add string_set "d"
 let string_set = add string_set "e"
-
- let even x = x mod 2 = 0
+let even x = x mod 2 = 0
 
 let test_add _ =
-    assert_equal true (setHas set 1);
-    assert_equal true (setHas set 2);
-    assert_equal true (setHas set 3);
-    assert_equal true (setHas set 4);
-    assert_equal true (setHas set 5);
-    assert_equal true (setHas string_set "a");
-    assert_equal true (setHas string_set "b");
-    assert_equal true (setHas string_set "c");
-    assert_equal true (setHas string_set "d");
-    assert_equal true (setHas string_set "e")
+  assert_equal true (setHas set 1);
+  assert_equal true (setHas set 2);
+  assert_equal true (setHas set 3);
+  assert_equal true (setHas set 4);
+  assert_equal true (setHas set 5);
+  assert_equal true (setHas string_set "a");
+  assert_equal true (setHas string_set "b");
+  assert_equal true (setHas string_set "c");
+  assert_equal true (setHas string_set "d");
+  assert_equal true (setHas string_set "e")
 
+let test_remove _ =
+  let set = remove set 1 in
+  let set = remove set 2 in
+  let set = remove set 3 in
+  let set = remove set 4 in
+  let set = remove set 5 in
+  assert_equal false (setHas set 1);
+  assert_equal false (setHas set 2);
+  assert_equal false (setHas set 3);
+  assert_equal false (setHas set 4);
+  assert_equal false (setHas set 5);
+  let string_set = remove string_set "a" in
+  let string_set = remove string_set "b" in
+  let string_set = remove string_set "c" in
+  let string_set = remove string_set "d" in
+  let string_set = remove string_set "e" in
+  assert_equal false (setHas string_set "a");
+  assert_equal false (setHas string_set "b");
+  assert_equal false (setHas string_set "c");
+  assert_equal false (setHas string_set "d");
+  assert_equal false (setHas string_set "e")
 
- let test_remove _ =
-    let set = remove set 1 in
-    let set = remove set 2 in
-    let set = remove set 3 in
-    let set = remove set 4 in
-    let set = remove set 5 in
-    assert_equal false (setHas set 1);
-    assert_equal false (setHas set 2);
-    assert_equal false (setHas set 3);
-    assert_equal false (setHas set 4);
-    assert_equal false (setHas set 5);
-    let string_set = remove string_set "a" in
-    let string_set = remove string_set "b" in
-    let string_set = remove string_set "c" in
-    let string_set = remove string_set "d" in
-    let string_set = remove string_set "e" in
-    assert_equal false (setHas string_set "a");
-    assert_equal false (setHas string_set "b");
-    assert_equal false (setHas string_set "c");
-    assert_equal false (setHas string_set "d");
-    assert_equal false (setHas string_set "e")
-
-
- let test_filter _ =
-    let set = filter set even in
-    assert_equal false (setHas set 1);
-    assert_equal true (setHas set 2);
-    assert_equal false (setHas set 3);
-    assert_equal true (setHas set 4);
-    assert_equal false (setHas set 5);
-    let string_set = filter string_set (fun x -> x = "a" || x = "c" || x = "e") in
-    assert_equal true (setHas string_set "a");
-    assert_equal false (setHas string_set "b");
-    assert_equal true (setHas string_set "c");
-    assert_equal false (setHas string_set "d");
-    assert_equal true (setHas string_set "e")
+let test_filter _ =
+  let set = filter set even in
+  assert_equal false (setHas set 1);
+  assert_equal true (setHas set 2);
+  assert_equal false (setHas set 3);
+  assert_equal true (setHas set 4);
+  assert_equal false (setHas set 5);
+  let string_set = filter string_set (fun x -> x = "a" || x = "c" || x = "e") in
+  assert_equal true (setHas string_set "a");
+  assert_equal false (setHas string_set "b");
+  assert_equal true (setHas string_set "c");
+  assert_equal false (setHas string_set "d");
+  assert_equal true (setHas string_set "e")
 
 let test_map _ =
-    let set = map (fun x -> x * 2) set  in
-    assert_equal false (setHas set 1);
-    assert_equal true (setHas set 2);
-    assert_equal false (setHas set 3);
-    assert_equal true (setHas set 4);
-    assert_equal false (setHas set 5);
-    assert_equal true (setHas set 6);
-    assert_equal true (setHas set 8);
-    assert_equal true (setHas set 10);
-    let string_set = map (fun x -> x ^ x) string_set in
-    assert_equal true (setHas string_set "aa");
-    assert_equal true (setHas string_set "bb");
-    assert_equal true (setHas string_set "cc");
-    assert_equal true (setHas string_set "dd");
-    assert_equal true (setHas string_set "ee")
+  let set = map (fun x -> x * 2) set in
+  assert_equal false (setHas set 1);
+  assert_equal true (setHas set 2);
+  assert_equal false (setHas set 3);
+  assert_equal true (setHas set 4);
+  assert_equal false (setHas set 5);
+  assert_equal true (setHas set 6);
+  assert_equal true (setHas set 8);
+  assert_equal true (setHas set 10);
+  let string_set = map (fun x -> x ^ x) string_set in
+  assert_equal true (setHas string_set "aa");
+  assert_equal true (setHas string_set "bb");
+  assert_equal true (setHas string_set "cc");
+  assert_equal true (setHas string_set "dd");
+  assert_equal true (setHas string_set "ee")
 
 let test_fold_left _ =
-    let sum = fold_left (fun acc x -> acc + x) 0 set in
-    assert_equal 15 sum;
-    let concat = fold_left (fun acc x -> acc ^ x) "" string_set
-    in
-    assert_equal "eabcd" concat
+  let sum = fold_left (fun acc x -> acc + x) 0 set in
+  assert_equal 15 sum;
+  let concat = fold_left (fun acc x -> acc ^ x) "" string_set in
+  assert_equal "eabcd" concat
 
-
- let test_fold_right _ =
-    let sum = fold_right (fun x acc -> acc + x) 0 set in
-    assert_equal 15 sum;
-    let concat = fold_right (fun x acc -> acc ^ x) "" string_set in
-    assert_equal "dcbae" concat
+let test_fold_right _ =
+  let sum = fold_right (fun x acc -> acc + x) 0 set in
+  assert_equal 15 sum;
+  let concat = fold_right (fun x acc -> acc ^ x) "" string_set in
+  assert_equal "dcbae" concat
 
 let test_merge _ =
-    let set1 = create len in
-    let set1 = add set1 1 in
-    let set1 = add set1 2 in
-    let set1 = add set1 3 in
-    let set1 = add set1 4 in
-    let set1 = add set1 5 in
-    let set2 = create len in
-    let set2 = add set2 6 in
-    let set2 = add set2 7 in
-    let set2 = add set2 8 in
-    let set2 = add set2 9 in
-    let set2 = add set2 10 in
-    let set = merge set1 set2 in
-    Printf.printf "%s\n" (string_of_hashSet set);
-    assert_equal true (setHas set 1);
-    assert_equal true (setHas set 2);
-    assert_equal true (setHas set 3);
-    assert_equal true (setHas set 4);
-    assert_equal true (setHas set 5);
-    assert_equal true (setHas set 6);
-    assert_equal true (setHas set 7);
-    assert_equal true (setHas set 8);
-    assert_equal true (setHas set 9);
-    assert_equal true (setHas set 10)
+  let set1 = create len in
+  let set1 = add set1 1 in
+  let set1 = add set1 2 in
+  let set1 = add set1 3 in
+  let set1 = add set1 4 in
+  let set1 = add set1 5 in
+  let set2 = create len in
+  let set2 = add set2 6 in
+  let set2 = add set2 7 in
+  let set2 = add set2 8 in
+  let set2 = add set2 9 in
+  let set2 = add set2 10 in
+  let set = merge set1 set2 in
+  Printf.printf "%s\n" (string_of_hashSet set);
+  assert_equal true (setHas set 1);
+  assert_equal true (setHas set 2);
+  assert_equal true (setHas set 3);
+  assert_equal true (setHas set 4);
+  assert_equal true (setHas set 5);
+  assert_equal true (setHas set 6);
+  assert_equal true (setHas set 7);
+  assert_equal true (setHas set 8);
+  assert_equal true (setHas set 9);
+  assert_equal true (setHas set 10)
 
 let test_compare _ =
-    let set1 = create len in
-    let set1 = add set1 1 in
-    let set1 = add set1 2 in
-    let set1 = add set1 3 in
-    let set1 = add set1 4 in
-    let set1 = add set1 5 in
-    let set2 = create len in
-    let set2 = add set2 6 in
-    let set2 = add set2 7 in
-    let set2 = add set2 8 in
-    let set2 = add set2 9 in
-    let set2 = add set2 10 in
-    assert_equal false (compare_sets set1 set2);
-    assert_equal true (compare_sets set1 set)
+  let set1 = create len in
+  let set1 = add set1 1 in
+  let set1 = add set1 2 in
+  let set1 = add set1 3 in
+  let set1 = add set1 4 in
+  let set1 = add set1 5 in
+  let set2 = create len in
+  let set2 = add set2 6 in
+  let set2 = add set2 7 in
+  let set2 = add set2 8 in
+  let set2 = add set2 9 in
+  let set2 = add set2 10 in
+  assert_equal false (compare_sets set1 set2);
+  assert_equal true (compare_sets set1 set)
 
 let test_associative _ =
-    let set1 = create 10 in
-    let set1 = add set1 "a" in
-    let set1 = add set1 "b" in  (* Переприсваиваем результат *)
-    let set2 = create 10 in
-    let set2 = add set2 "c" in
-    let set2 = add set2 "d" in
-    let set3 = create 10 in
-    let set3 = add set3 "e" in
-    let test_assoc = merge (merge set1 set2) set3 = merge set1 (merge set2 set3) in
-    assert_equal true test_assoc
+  let set1 = create 10 in
+  let set1 = add set1 "a" in
+  let set1 = add set1 "b" in
+  let set2 = create 10 in
+  let set2 = add set2 "c" in
+  let set2 = add set2 "d" in
+  let set3 = create 10 in
+  let set3 = add set3 "e" in
+  let test_assoc =
+    merge (merge set1 set2) set3 = merge set1 (merge set2 set3)
+  in
+  assert_equal true test_assoc
 
 let test_commutative _ =
-    let set1 = create 10 in
-    let set1 = add set1 "a" in
-    let set1 = add set1 "b" in  (* Переприсваиваем результат *)
-    let set2 = create 10 in
-    let set2 = add set2 "c" in
-    let set2 = add set2 "d" in
-    let test_comm = merge set1 set2 = merge set2 set1 in
-    assert_equal true test_comm
+  let set1 = create 10 in
+  let set1 = add set1 "a" in
+  let set1 = add set1 "b" in
+  let set2 = create 10 in
+  let set2 = add set2 "c" in
+  let set2 = add set2 "d" in
+  let test_comm = merge set1 set2 = merge set2 set1 in
+  assert_equal true test_comm
 
+let suite =
+  "test_second_lab"
+  >::: [
+         "test_add" >:: test_add;
+         "test_remove" >:: test_remove;
+         "test_filter" >:: test_filter;
+         "test_map" >:: test_map;
+         "test_fold_left" >:: test_fold_left;
+         "test_fold_right" >:: test_fold_right;
+         "test_merge" >:: test_merge;
+         "test_compare" >:: test_compare;
+         "test_associative" >:: test_associative;
+         "test_commutative" >:: test_commutative;
+       ]
 
-
-    (* Тест на идемпотентность *)
-let suite = "test_second_lab" >::: [
-    "test_add" >:: test_add;
-    "test_remove" >:: test_remove;
-    "test_filter" >:: test_filter;
-    "test_map" >:: test_map;
-    "test_fold_left" >:: test_fold_left;
-    "test_fold_right" >:: test_fold_right;
-    "test_merge" >:: test_merge;
-    "test_compare" >:: test_compare;
-    "test_associative" >:: test_associative;
-    "test_commutative" >:: test_commutative
-]
-
-let () =
-    run_test_tt_main suite
+let () = run_test_tt_main suite
